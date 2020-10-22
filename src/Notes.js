68e6637480e395ba7656521933a13b900bfb21f2
dedkovnd <dedkovnd@gmail.com>
2020-10-22 16:56:23 +0300

    add delete function, fixed button click

--- src/Notes.js
+++ src/Notes.js
@@ -14,6 +14,7 @@ export class Notes extends React.Component {
             .then(json=>this.setState({notes: json}))
     }
 
+
     updateButton(evt) {
         evt.preventDefault()
         fetch('http://localhost:7777/notes')
@@ -23,18 +24,30 @@ export class Notes extends React.Component {
 
     deleteButton(evt){
         evt.preventDefault()
-        console.log(evt.target.value)
-     }
+        const note = evt.target.previousSibling.innerText
+        this.state.notes.forEach(res=> {
+          if(res.content === note) {
+              fetch(`http://localhost:7777/notes/${res.id}`, {
+                  method:'DELETE'
+              })
+              fetch('http://localhost:7777/notes')
+                  .then(response=> response.json())
+                  .then(json=>this.setState({notes: json}))
+          }
+        })
+        console.log(this.state)
+    }
 
-     render(){
+    render(){
         console.log(this.state)
         const {notes} = this.state
         return <div>
             {notes.map((note, i)=>
-                <div key={i}>{note.content}
-                <button type="submit" onSubmit={this.deleteButton}>Delete</button>
+                <div key={i}>
+                    <div>{note.content}</div>
+                <button onClick={this.deleteButton}>Delete</button>
                 </div>)}
-                <button type="submit" onSubmit={this.updateButton}>Update</button>
+                <button onClick={this.updateButton}>Update</button>
         </div>
     }
 }
